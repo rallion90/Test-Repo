ecd966f04d3d06c7a3badaa9f37f29bf41506b05
Rallion Laynes <ralphlaynes@gmail.com>
2022-07-18 13:52:32 +0800

    cnfig

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <button className="bg-sky-600 hover:bg-sky-700">
+        Save changes
+    </button>
+  )
+}
+
+export default Sidebar
